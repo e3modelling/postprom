148e83cd93459cce2cf49d99b330c2b088ebc8fc
Michael Madianos <mixalisspa@gmail.com>
2025-05-23 13:00:16 +0300

    Refactored reportCapacityAdditions.R

--- R/reportCapacityAdditions.R
+++ R/reportCapacityAdditions.R
@@ -5,10 +5,18 @@
 #' @export
 reportCapacityAdditions <- function(path, regions, years) {
   VNewCapElec <- readGDX(path, "VNewCapElec", field = "l")[regions, years, ]
-
+  VNetNewCapElec <- readGDX(path, "VNetNewCapElec", field = "l")[regions, years, ]
   PGALLtoEF <- readGDX(path, "PGALLtoEF")
   names(PGALLtoEF) <- c("PGALL", "EF")
 
+  magpie_object <- helper(VNewCapElec, PGALLtoEF, "Capacity Additions")
+  magpie_object <- mbind(magpie_object, helper(VNetNewCapElec, PGALLtoEF, "Net Capacity Additions"))
+  return(magpie_object)
+}
+
+# Helper ----------------------------------------------------------------------------------------
+helper <- function(variable, PGALLtoEF, title) {
+  title <- paste0(title, "|Electricity|")
   add_LGN <- as.data.frame(PGALLtoEF[which(PGALLtoEF[, 2] == "LGN"), 1])
   add_LGN["EF"] <- "Lignite"
   names(add_LGN) <- c("PGALL", "EF")
@@ -28,33 +36,32 @@ reportCapacityAdditions <- function(path, regions, years) {
   )
   PGALLtoEF$EF <- str_replace_all(PGALLtoEF$EF, rename_EF)
 
-  VNewCapElec_without_aggr <- VNewCapElec
-  getItems(VNewCapElec_without_aggr, 3) <- paste0("Capacity Additions|Electricity|", getItems(VNewCapElec_without_aggr, 3))
+  var_without_aggr <- variable
+  getItems(var_without_aggr, 3) <- paste0(title, getItems(var_without_aggr, 3))
 
   magpie_object <- NULL
-  VNewCapElec_without_aggr <- add_dimension(VNewCapElec_without_aggr, dim = 3.2, add = "unit", nm = "GW/yr")
-  magpie_object <- mbind(magpie_object, VNewCapElec_without_aggr)
+  var_without_aggr <- add_dimension(var_without_aggr, dim = 3.2, add = "unit", nm = "GW/yr")
+  magpie_object <- mbind(magpie_object, var_without_aggr)
 
 
   # aggregate to reporting fuel categories
-  VNewCapElec_LGN <- toolAggregate(VNewCapElec[, , add_LGN[["PGALL"]]], dim = 3, rel = add_LGN, from = "PGALL", to = "EF")
-  VNewCapElec <- toolAggregate(VNewCapElec[, , PGALLtoEF[["PGALL"]]], dim = 3, rel = PGALLtoEF, from = "PGALL", to = "EF")
+  var_LGN <- toolAggregate(variable[, , add_LGN[["PGALL"]]], dim = 3, rel = add_LGN, from = "PGALL", to = "EF")
+  variable <- toolAggregate(variable[, , PGALLtoEF[["PGALL"]]], dim = 3, rel = PGALLtoEF, from = "PGALL", to = "EF")
 
-  getItems(VNewCapElec_LGN, 3) <- paste0("Capacity Additions|Electricity|", getItems(VNewCapElec_LGN, 3))
+  getItems(var_LGN, 3) <- paste0(title, getItems(var_LGN, 3))
 
-  VNewCapElec_LGN <- add_dimension(VNewCapElec_LGN, dim = 3.2, add = "unit", nm = "GW/yr")
-  magpie_object <- mbind(magpie_object, VNewCapElec_LGN)
+  var_LGN <- add_dimension(var_LGN, dim = 3.2, add = "unit", nm = "GW/yr")
+  magpie_object <- mbind(magpie_object, var_LGN)
 
-  getItems(VNewCapElec, 3) <- paste0("Capacity Additions|Electricity|", getItems(VNewCapElec, 3))
+  getItems(variable, 3) <- paste0(title, getItems(variable, 3))
 
-  VNewCapElec <- add_dimension(VNewCapElec, dim = 3.2, add = "unit", nm = "GW/yr")
-  magpie_object <- mbind(magpie_object, VNewCapElec)
+  variable <- add_dimension(variable, dim = 3.2, add = "unit", nm = "GW/yr")
+  magpie_object <- mbind(magpie_object, variable)
 
   # electricity production
-  VNewCapElec_total <- dimSums(VNewCapElec, dim = 3, na.rm = TRUE)
-  getItems(VNewCapElec_total, 3) <- "Capacity Additions|Electricity"
+  var_total <- dimSums(variable, dim = 3, na.rm = TRUE)
+  getItems(var_total, 3) <- title
 
-  VNewCapElec_total <- add_dimension(VNewCapElec_total, dim = 3.2, add = "unit", nm = "GW/yr")
-  magpie_object <- mbind(magpie_object, VNewCapElec_total)
-  return(magpie_object)
+  var_total <- add_dimension(var_total, dim = 3.2, add = "unit", nm = "GW/yr")
+  magpie_object <- mbind(magpie_object, var_total)
 }
