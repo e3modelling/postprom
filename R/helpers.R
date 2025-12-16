29e3fc9fd1c2c4bba10447b0a17f5a32fe4e8fb5
Michael Madianos <mixalisspa@gmail.com>
2025-12-16 16:16:01 +0200

    Aggregate helper function can now keep the intermediate aggregation levels.

--- R/helpers.R
+++ R/helpers.R
@@ -5,7 +5,8 @@ helperRenameItems <- function(magpie, mapping, prefix, dim = 3.1) {
   # Rename magpie from OPEN-PROM Efs to labels (e.g. Gas -> Secondary Energy|Electricity|)
   # and aggregate in case of duplicates (Coal from lignite and Hard coal to single Coal)
   magpie <- toolAggregate(
-    magpie, dim = 3, rel = mapping, from = "Tech", to = "EF"
+    magpie,
+    dim = 3, rel = mapping, from = "Tech", to = "EF"
   )
 
   name <- getItems(magpie, dim)
@@ -15,30 +16,41 @@ helperRenameItems <- function(magpie, mapping, prefix, dim = 3.1) {
 }
 
 #' @export
-helperAggregateLevel <- function(magpie_object, level) {
+helperAggregateLevel <- function(magpie_object, level, dim = 3.1, recursive = FALSE) {
   # Aggregate a dimension based on names' levels:
   # Secondary Energy|Electricity|Gas|w/o CCS, Secondary Energy|Electricity|Gas|w/ CCS
   # to Secondary Energy|Electricity|Gas
   if (!is.magpie(magpie_object)) stop("Input must be a magpie object")
   if (!is.numeric(level) || level < 1) stop("Level must be a positive integer")
 
-  full_names <- getItems(magpie_object, dim = 3)
-  split_names <- strsplit(full_names, "\\|")
-  keep_idx <- sapply(split_names, length) == level
+  full_names <- getItems(magpie_object, dim = dim)
+  max_depth <- max(sapply(strsplit(full_names, "\\|"), length))
 
-  if (!any(keep_idx)) {
-    warning(paste("No entries found with exactly", level, "levels."))
-    return(NULL)
+  if (level >= max_depth) {
+    return(magpie_object)
   }
-  x <- magpie_object[,,keep_idx]
 
-  parent_names <- sapply(split_names[keep_idx], function(x) paste(x[1:level-1], collapse = "|"))
+  if (recursive == TRUE) {
+    map <- NULL
+    for (lvl in seq(max_depth, level, -1)) {
+      pattern <- paste0("^([^|]+\\|){", lvl - 1, "}[^|]+")
+      new_names <- str_extract(full_names, pattern)
+      temp_map <- data.frame(names = full_names, parent = new_names)
+      map <- bind_rows(map, temp_map)
+    }
+  } else {
+    # keep name up to the level of depth
+    pattern <- paste0("^([^|]+\\|){", level - 1, "}[^|]+")
+    new_names <- str_extract(full_names, pattern)
 
-  map <- data.frame(names=full_names[keep_idx], parent=parent_names)
+    # Aggregate based on new level structure
+    map <- data.frame(names = full_names, parent = new_names)
+  }
 
-  # Aggregate across identical parent names
+  map <- map %>% filter(!is.na(parent))
   x <- toolAggregate(
-    x, dim = 3, rel = map, from = "names", to = "parent"
+    magpie_object,
+    dim = dim, rel = map, from = "names", to = "parent"
   )
   return(x)
 }
