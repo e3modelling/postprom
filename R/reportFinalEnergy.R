1185a45d7757e8be809984b67b352a83f1643323
Fsioutas <fsioutas@hotmail.com>
2026-02-09 10:58:31 +0200

    Final Energy, Final Energy|wo/Bunkers

--- R/reportFinalEnergy.R
+++ R/reportFinalEnergy.R
@@ -75,7 +75,7 @@ reportFinalEnergy <- function(path, regions, years) {
   getItems(VConsFinEneCountry, 3) <- paste0("Final Energy|", getItems(VConsFinEneCountry, 3))
 
   l <- getNames(VConsFinEneCountry) == "Final Energy|Total"
-  getNames(VConsFinEneCountry)[l] <- "Final Energy"
+  getNames(VConsFinEneCountry)[l] <- "Final Energy|wo/Bunkers"
 
   VConsFinEneCountry <- add_dimension(VConsFinEneCountry, dim = 3.2, add = "unit", nm = "Mtoe")
 
@@ -91,6 +91,8 @@ reportFinalEnergy <- function(path, regions, years) {
 
   # variables of OPEN-PROM related to sectors
   blabla_var <- c("VmDemFinEneTranspPerFuel", "VmConsFuel", "VmConsFuel", "VmConsFuel", "VmConsFuel")
+  
+  Final_Energy_with_Bunkers <- NULL
 
   for (y in 1:length(sector)) {
     # read GAMS set used for reporting of Final Energy different for each sector
@@ -125,6 +127,10 @@ reportFinalEnergy <- function(path, regions, years) {
 
     sector_open <- add_dimension(sector_open, dim = 3.2, add = "unit", nm = "Mtoe")
     magpie_object <- mbind(magpie_object, sector_open)
+    
+    # Final Energy|w/Bunkers
+    
+    Final_Energy_with_Bunkers <- mbind(Final_Energy_with_Bunkers, sector_open)
 
     # Energy Forms Aggregations
     sets5 <- readGDX(path, "EFtoEFA")
@@ -217,6 +223,15 @@ reportFinalEnergy <- function(path, regions, years) {
     
     magpie_object <- mbind(magpie_object, FCONS_per_fuel_OP_new_names)
   }
+  
+  ################## Final_Energy_with_Bunkers
+  ################## Final Energy Total
+  Final_Energy_with_Bunkers <- dimSums(Final_Energy_with_Bunkers, dim = 3, na.rm = TRUE)
+  getItems(Final_Energy_with_Bunkers, 3) <- paste0("Final Energy")
+
+  Final_Energy_with_Bunkers <- add_dimension(Final_Energy_with_Bunkers, dim = 3.2, add = "unit", nm = "Mtoe")
+  magpie_object <- mbind(magpie_object, Final_Energy_with_Bunkers)
+  
   
   #########       DAC     #################
   
