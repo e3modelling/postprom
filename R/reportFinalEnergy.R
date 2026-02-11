4af4d67a8ac4f382e21f1c78884c2d972b097d05
Michael Madianos <mixalisspa@gmail.com>
2026-02-11 13:23:13 +0200

    Complete final energy

--- R/reportFinalEnergy.R
+++ R/reportFinalEnergy.R
@@ -19,62 +19,42 @@
 #' @importFrom stringr str_replace_all
 #' @export
 reportFinalEnergy <- function(path, regions, years) {
+  EFSTable <- rgdx.set(path, "EFS", te = TRUE)
+  DSBSTable <- rgdx.set(path, "DSBS", te = TRUE)
+
+  #---------- Create a DSBS TO SBS mapping (e.g., Iron & Steel -> Industry) -----
   DSBS_Industry <- readGDX(path, "INDSE") %>%
-    as.data.frame() %>% 
-    mutate(SBS = "Industry") 
+    as.data.frame() %>%
+    mutate(SBS = "Industry")
   DSBS_Transport <- readGDX(path, "TRANSE") %>%
     as.data.frame() %>%
     mutate(SBS = "Transportation")
-  DSBS_NonEnergy <- readGDX(path,"NENSE") %>%
+  DSBS_NonEnergy <- readGDX(path, "NENSE") %>%
     as.data.frame() %>%
     filter(. != "BU") %>%
     mutate(SBS = "Non-Energy Use")
   DSBS_Rest <- data.frame(
-    DSBS = c("HOU","AG", "SE", "BU", "DAC")) %>%
+    DSBS = c("HOU", "AG", "SE", "BU", "DAC")
+  ) %>%
     mutate(SBS = "Temp")
-  DSBS_SBS = bind_rows(DSBS_Industry, DSBS_Transport, DSBS_NonEnergy) %>%
+  DSBS_SBS <- bind_rows(DSBS_Industry, DSBS_Transport, DSBS_NonEnergy) %>%
     rename(DSBS = 1)
-  DSBStoSBS = bind_rows(DSBS_SBS,DSBS_Rest)
+  DSBStoSBS <- bind_rows(DSBS_SBS, DSBS_Rest) %>%
+    left_join(DSBSTable, by = c("DSBS" = "SBS")) %>%
+    select(-DSBS) %>%
+    rename(DSBS = .te)
   lookup <- setNames(DSBStoSBS$SBS, DSBStoSBS$DSBS)
 
-  rel <- data.frame(
-    code = c(
-      "HCL", "LGN", "CRO", "LPG", "GSL", "KRS", "GDO", "RFO", "OLQ", "NGS", "OGS", "NUC", "STE",
-      "HYD", "WND", "SOL", "BMSWAS", "GEO", "MET", "ETH", "BGDO", "BGSL", "H2F", "ELC"
-    ),
-    description = c(
-      "Hard Coal-Coke-Other Solids",
-      "Lignite",
-      "Crude Oil and Feedstocks",
-      "Liquefied Petroleum Gas",
-      "Gasoline",
-      "Kerosene",
-      "Diesel Oil",
-      "Residual Fuel Oil",
-      "Other Liquids",
-      "Natural Gas",
-      "Other Gases",
-      "Nuclear",
-      "Heat",
-      "Hydro",
-      "Wind",
-      "Solar",
-      "Biomass and Waste",
-      "Geothermal and other renewable sources",
-      "Methanol",
-      "Ethanol",
-      "Biodiesel",
-      "Biogasoline",
-      "Hydrogen",
-      "Electricity"
-    ),
-    stringsAsFactors = FALSE
-  ) %>% rename(Tech = code, EF = description)
-
+  # -------------------------- Prepare data --------------------------------------
   fuel <- readGDX(path, "VmConsFuel", field = "l")[regions, years, ]
   VFuelTransport <- readGDX(path, "VmDemFinEneTranspPerFuel", field = "l")[regions, years, ]
   fuel[, , getItems(VFuelTransport, 3)] <- VFuelTransport[, , getItems(VFuelTransport, 3)]
-  fuel <- fuel[, , rel$Tech]
+  fuel <- fuel[, , EFSTable$EF]
+
+  # Rename Sectors
+  getItems(fuel, 3.1) <- DSBSTable$.te[match(getItems(fuel, 3.1), DSBSTable$SBS)]
+  # Rename Fuels
+  getItems(fuel, 3.2) <- EFSTable$.te[match(getItems(fuel, 3.2), EFSTable$EF)]
 
   # Replace sep in dimensions and prepend the sector
   name <- gsub("\\.", "|", getItems(fuel, dim = 3)) # IS.HCL --> IS|HCL
@@ -83,7 +63,7 @@ reportFinalEnergy <- function(path, regions, years) {
     "^[^|]+",
     paste0(lookup[str_extract(name, "^[^|]+")], "|\\0")
   ) # prepend SBS (e.g., IS|HCL -> Industry|IS|HCL)
-  name <- str_replace(name,"Temp\\|", "")
+  name <- str_replace(name, "Temp\\|", "")
   prefix <- "Final Energy|"
   title <- if (!is.null(prefix)) paste0(prefix, name) else name
   getItems(fuel, 3) <- title
