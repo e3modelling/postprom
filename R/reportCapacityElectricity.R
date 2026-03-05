e4a3ec7f4ff039b2932424299ea16c55bc15e774
Fsioutas <fsioutas@hotmail.com>
2026-03-05 17:52:32 +0200

    fix capac elec

--- R/reportCapacityElectricity.R
+++ R/reportCapacityElectricity.R
@@ -15,55 +15,100 @@
 #' @importFrom gdx readGDX
 #' @importFrom magclass getItems add_dimension mbind dimSums
 #' @importFrom madrat toolAggregate
-#' @importFrom dplyr %>% rename
+#' @importFrom dplyr %>% rename coalesce group_by summarise
 #' @export
 reportCapacityElectricity <- function(path, regions, years) {
-  rename_EF <- c(
-    "LGN" = "Coal",
-    "HCL" = "Coal",
-    "GDO" = "Oil",
-    "NGS" = "Gas",
-    "BMSWAS" = "Biomass",
-    "NUC" = "Nuclear",
-    "HYD" = "Hydro",
-    "WND" = "Wind",
-    "SOL" = "Solar",
-    "GEO" = "Geothermal",
-    "H2F" = "Hydrogen",
-    "STE" = "Steam"
-  )
-  mapCCS <- readGDX(path, "CCS_NOCCS") %>%
-    as.data.frame() %>%
-    rename(CCS = PGALL, NOCCS = PGALL1)
 
-  PGALLtoEF <- readGDX(path, "PGALLtoEF") %>%
-    rename(Tech = PGALL, EF = EFS)
-  CCStoEF <- PGALLtoEF %>%
-    filter(Tech %in% mapCCS$CCS)
-
-  PGALLtoEF$EF <- str_replace_all(PGALLtoEF$EF, rename_EF)
-  CCStoEF$EF <- str_replace_all(CCStoEF$EF, rename_EF)
-
-  CCS <- PGALLtoEF %>%
-    filter(Tech %in% CCStoEF$Tech) %>%
-    select(Tech) %>%
-    unlist(use.names = FALSE)
-
+  BALEFtoEF <- read.csv(
+    system.file("mappings", "BALEFtoEF.csv", package = "postprom")
+  ) %>%
+    separate_rows(EF) %>%
+    filter(BALEF %in% c(
+      "Coal", "Gas", "Nuclear", "Biofuels", "Oil",
+      "Solar", "Wind", "Geothermal", "Hydrogen", "Hydro"
+    ))
+  
+  PGALLtoEF <- readGDX(path, "PGALLtoEF")
+  
+  sets <- full_join(PGALLtoEF %>% rename(EF = EFS), BALEFtoEF, by = "EF")
+  
   capacity <- readGDX(path, "V04CapElecNominal",
-    field = "l"
-  )[regions, years, ]
+                               field = "l")[regions, years, ]
+  
+  i04ShareFuels <- readGDX(path, "i04ShareFuels", field = "l")[regions,, ]
+  i04ShareFuels <- i04ShareFuels[, , paste0(PGALLtoEF$PGALL, ".", PGALLtoEF$EFS)]
+  PGALLtoEF[, 2] <- paste0(PGALLtoEF$PGALL, ".", PGALLtoEF$EFS)
+  
+  capacity <- toolAggregate(capacity, dim = 3, weight = i04ShareFuels,
+                            rel = PGALLtoEF, from = "PGALL", to = "EFS"
+  )
+  
+  prefix <- sub("\\..*", "", getItems(capacity, 3.1))
+  withCCS <- case_when(
+    prefix %in% readGDX(path, "CCS") ~ "w/ CCS",
+    prefix %in% c(readGDX(path, "NOCCS"), "ATHOIL") ~ "w/o CCS",
+    TRUE ~ ""
+  )
+  
+  HYD <- case_when(
+    prefix %in% "PGLHYD" ~ "Large",
+    prefix %in% "PGSHYD" ~ "Small",
+    TRUE ~ ""
+  )
+  
+  WND <- case_when(
+    prefix %in% "PGAWND" ~ "Onshore",
+    prefix %in% "PGAWNO" ~ "Offshore",
+    TRUE ~ ""
+  )
+  
+  SOL <- case_when(
+    prefix %in% "PGCSP" ~ "CSP",
+    prefix %in% "PGSOL" ~ "PV",
+    TRUE ~ ""
+  )
+  
+  tech <- coalesce(
+    na_if(SOL, ""),
+    na_if(WND, ""),
+    na_if(HYD, ""),
+    na_if(withCCS, "")
+  )
+  
+  sets <- filter(sets, PGALL %in% getItems(capacity, 3.1))
+  
+  getItems(capacity, 3.2) <- sets$BALEF[match(getItems(capacity, 3.2), sets$EF)]
+  
+  x1 <- as.data.frame(getItems(capacity, 3.1))
+  x2 <- as.data.frame(tech)
+  
+  tech_join <- cbind(x1, x2)
+  names(tech_join) <- c("pg", "tech")
+  
+  getItems(capacity, 3.1) <- tech_join$tech[match(getItems(capacity, 3.1), tech_join$pg)]
+  
+  capacity <- as.quitte(capacity)
+  
+  capacity <- capacity %>%
+    group_by(region, PGALL, data, period) %>%
+    summarise(value = sum(value, na.rm = TRUE), .groups = "drop")  %>%
+    select(region, period, value, PGALL, data) %>%
+    rename(variable = data, tech = PGALL) %>%
+    as.quitte() %>% as.magpie()
 
-  mapping <- PGALLtoEF %>%
-    mutate(
-      EF = ifelse(EF %in% c("Hard coal", "Lignite"), "Coal", EF),
-      EF = ifelse(Tech %in% CCS, paste0(EF, "|w/ CCS"), EF),
-      EF = ifelse(EF %in% CCStoEF$EF & !(Tech %in% CCS), paste0(EF, "|w/o CCS"), EF)
-    )
-
-  prefix <- "Capacity|Electricity|"
-  capacity <- helperRenameItems(capacity, mapping = mapping, prefix = prefix)
-  capAll <- helperAggregateLevel(capacity, level = 2, recursive = TRUE)
-
-  capAll <- add_dimension(capAll, dim = 3.2, add = "unit", nm = "GW")
-  return(capAll)
+  
+  name <- gsub("\\.", "|", sub("\\.NA$", "", getItems(capacity, dim = 3)))
+  getItems(capacity, 3) <- name
+  # =============================
+  
+  names(dimnames(capacity))[3.1] <- "variable"
+  
+  getItems(capacity, 3) <- paste0("Capacity|Electricity|", getItems(capacity, 3))
+  
+  capacity <- helperAggregateLevel(capacity, level = 1, recursive = TRUE)
+  
+  capacity <- add_dimension(capacity, dim = 3.2, add = "unit", nm = "GW")
+  
+  return(capacity)
 }
+  
