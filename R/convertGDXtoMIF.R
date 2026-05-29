56cfb31900ba2eaffa060b02f2a98627de8afe10
MargaritaEfthymiadou <margarita.efthymiadou@ricardo.com>
2026-05-29 11:04:14 +0100

    convertGDXtoMIF bug

--- R/convertGDXtoMIF.R
+++ R/convertGDXtoMIF.R
@@ -102,6 +102,7 @@ convertGDXtoMIF_single <- function(.path, path_mif, append, regions = NULL,
   reports <- mbind(reports, reportCapacityAdditions(path_gdx, regions, years))
   reports <- mbind(reports, reportCostsPGtechnologies(path_gdx, regions, years))
   reports <- mbind(reports, reportVehicles(path_gdx, regions, years))
+  reports <- mbind(reports, reportGrossInlandConsumption(path_gdx, regions, years))
   reports <- mbind(reports, reportEquipmentCapacityShare(path_gdx, regions, years))
   
 
