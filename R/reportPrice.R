5a895b33635207281b6e7b257dcdb0ee03a73961
Fsioutas <fsioutas@hotmail.com>
2026-06-24 18:46:38 +0300

    Bump version to 0.13.3 in DESCRIPTION and enhance reportPrice function for improved price aggregation

--- R/reportPrice.R
+++ R/reportPrice.R
@@ -53,6 +53,7 @@ reportPrice <- function(path, regions, years) {
   lookup <- setNames(DSBS_SBS$SBS, DSBS_SBS$DSBS)
   # -------------------------- Prepare data --------------------------------------
   prices <- readGDX(path, "VmPriceFuelSubsecCarVal", field = "l")[regions, years, DSBS]
+  pricesNoAgr <- prices
   years <- getYears(prices)
   units <- sub(".*\\((.*)\\).*", "\\1", prices@description)
   # -------------------------- Renamings ------------------------------
@@ -71,9 +72,60 @@ reportPrice <- function(path, regions, years) {
     name
   ) # prepend SBS (e.g., IS|HCL -> Industry|IS|HCL)
 
-  getItems(prices, 3) <- paste0("Prices|Final Energy|", name)
+  getItems(prices, 3) <- paste0("Price|Final Energy|", name)
   
-  prices <- add_dimension(prices, dim = 3.2, add = "unit", nm = "k$2015/toe")
+  prices <- add_dimension(prices, dim = 3.2, add = "unit", nm = units)
+  
+  BALEF2EFS <- readGDX(path, "BALEF2EFS")
+  names(BALEF2EFS) <- c("BAL", "EF")
+  
+  BALEF2EFS[["BAL"]] <- gsub("Gas fuels", "Gases", BALEF2EFS[["BAL"]])
+  BALEF2EFS[["BAL"]] <- gsub("Steam", "Heat", BALEF2EFS[["BAL"]])
+  
+  pricesNoAgr <- toolAggregate(pricesNoAgr, dim = 3.1, rel = DSBSTable, from = "SBS", to = ".te")
+  
+  DSBS_SBS_full <- bind_rows(
+    DSBS_Industry, DSBS_Transport,
+    DSBS_NonEnergy, DSBS_CDR, DSBS_COMM
+  ) %>%
+    rename(DSBS = 1) %>%
+    full_join(DSBSTable, by = c("DSBS" = "SBS")) %>%
+    mutate(SBS = coalesce(SBS, .te)) %>%
+    select(-DSBS)
+  
+  pricesNoAgr <- toolAggregate(pricesNoAgr, dim = 3.1, rel = DSBS_SBS_full, from = ".te", to = "SBS")
+  
+  # aggregate from fuels to reporting fuel categories
+  sum_open_prom <- pricesNoAgr %>%
+    as.quitte() %>%
+    left_join(BALEF2EFS, by = "EF") %>% ## add mapping
+    mutate(value = mean(value, na.rm = TRUE), .by = c("model", "scenario", "region",
+                                                      "unit","period","BAL", "SBS")) %>%
+    distinct() %>%
+    select(c("model","scenario","region","unit", "period","value", "SBS", "BAL")) %>%
+    distinct() %>%
+    drop_na() %>%
+    as.quitte() %>%
+    as.magpie()
+  
+  # complete names
+  getItems(sum_open_prom, 3.1) <- paste0("Price|Final Energy|", getItems(sum_open_prom, 3.1))
+  
+  # Replace sep in dimensions and prepend the sector
+  name <- gsub("\\.", "|", getItems(sum_open_prom, dim = 3)) # e.g., IS.HCL --> IS|HCL
+  key <- str_extract(name, "^[^|]+")
+  mapped <- lookup[key]
+  
+  name <- if_else(
+    !is.na(mapped),
+    str_replace(name, "^[^|]+", paste0(mapped, "|\\0")),
+    name
+  ) # prepend SBS (e.g., IS|HCL -> Industry|IS|HCL)
+  
+  getItems(sum_open_prom, 3) <- name
+  
+  sum_open_prom <- add_dimension(sum_open_prom, dim = 3.2, add = "unit", nm = units)
+  prices <- mbind(prices, sum_open_prom)
 
   return(prices)
 }
