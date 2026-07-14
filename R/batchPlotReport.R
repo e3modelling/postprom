fdefb7eb477472086b76c13c2fdc6004299cb67d
Fsioutas <fsioutas@hotmail.com>
2026-07-14 15:19:52 +0300

    Refactor reportAreaPNG function to simplify parameters and improve clarity by removing unused variables and updating documentation

--- R/batchPlotReport.R
+++ R/batchPlotReport.R
@@ -27,13 +27,6 @@ batchPlotReport <- function(report, metadata, save_pdf, PngFiles) {
   # for each unique plot, use filter the magpie obj and plot its vars
   grouped <- plot_mappings %>% group_by(Name)
   
-  regionsPNG <- c("World", "EU", "IND", "CHA", "USA")
-  yearsPNG <- c(2023, seq(2025, max(getYears(report, as.integer = TRUE)), by = 5))
-  
-  plotstyle <- read.csv(
-    system.file(package = "postprom", file.path("extdata", "plotstyle.csv"))
-  )
-  
   plots_list <- map2(
     group_split(grouped),
     group_keys(grouped)$Name,
@@ -53,14 +46,9 @@ batchPlotReport <- function(report, metadata, save_pdf, PngFiles) {
   output_path <- file.path(basename(dirname(output_path)), basename(output_path))
   opts_knit$set(base.dir = output_path)
   
-  output_PNG <- file.path(output_path, "PNG_area_plots")
-  
   if (PngFiles == TRUE) reportAreaPNG(report = report,
                                       grouped = grouped,
-                                      plotstyle = plotstyle,
-                                      regionsPNG = regionsPNG,
-                                      yearsPNG = yearsPNG,
-                                      output_dir = output_PNG)
+                                      output_dir = output_path)
 
   message(paste0("Saving pdf in ", sub("\\.tex$", ".pdf", save_pdf)))
   
@@ -98,33 +86,27 @@ plotGroups <- function(vars, name, report, ...) {
 #'
 #' @param report MAgPIE report object.
 #' @param grouped Data frame containing variable grouping information.
-#' @param plotstyle Data frame containing colors for each variable.
-#' @param regionsPNG Character vector of regions to plot.
-#' @param yearsPNG Numeric vector of years to plot.
-#' @param output_dir Output directory for the PNG files.
+#' @param output_dir Character string specifying the parent output directory.
 #'
 #' @return Invisibly returns NULL.
 #'
 #' @author Fotis Sioutas
 #'
-#' @importFrom quitte as.quitte
-#' @importFrom dplyr mutate filter left_join distinct pull rename select recode
-#' @importFrom stringr str_remove str_replace_all
-#' @importFrom ggplot2 ggplot aes geom_area facet_wrap labs scale_fill_manual
-#' @importFrom ggplot2 scale_x_continuous scale_y_continuous theme_bw theme
-#' @importFrom ggplot2 element_text element_rect element_blank guides guide_legend
-#' @importFrom ggplot2 expansion ggsave geom_hline scale_linetype_manual
-#' @importFrom scales pretty_breaks label_number
-#' @importFrom grid unit
+#' @importFrom dplyr filter group_split group_keys
+#' @importFrom purrr map2 imap
+#' @importFrom stringr str_replace_all
+#' @importFrom ggplot2 labs facet_wrap geom_area theme element_text ggsave
+#' @importFrom stats setNames
+#' 
 reportAreaPNG <- function(report,
                           grouped,
-                          plotstyle,
-                          regionsPNG,
-                          yearsPNG,
                           output_dir) {
   
+  regionsPNG <- c("World", "EU", "IND", "CHA", "USA")
+  yearsPNG <- c(2023, seq(2025, max(getYears(report, as.integer = TRUE)), by = 5))
   if (all(regionsPNG %in% getRegions(report))) {
     if (all(yearsPNG %in% as.numeric(sub("^y", "", getYears(report))))) {
+      output_dir <- file.path(output_dir, "PNG_area_plots")
       dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
       message("Saving png files")
       dataPlotPNG <- grouped[
@@ -136,254 +118,88 @@ reportAreaPNG <- function(report,
           trimws(grouped$Variables) != "",
       ]
       
-      dataPlotPNG <- dataPlotPNG %>%
-        mutate(Group = match(Name, unique(Name)))
+      dataPlotPNG <- dataPlotPNG %>% filter(!(Variables == "Demand"))
       
-      dataPlotPNG$Group <- cumsum(!duplicated(dataPlotPNG$Name))
+      magpiePNG <- report[regionsPNG, yearsPNG, ]
       
-      dataPlotPNGFilter <- dataPlotPNG %>%
-        mutate(
-          Name = paste0(
-            str_remove(Name, "\\|[^|]+$"),
-            "|",
-            Variables
-          )
-        )
+      plots_list <- map2(
+        group_split(dataPlotPNG),
+        group_keys(dataPlotPNG)$Name,
+        ~ plotGroups(.x$Variables, .y, magpiePNG)
+      ) %>% setNames(group_keys(dataPlotPNG)$Name)
       
-      dataPlotPNGFilter <- dataPlotPNGFilter %>%
-        filter(!(Group %in% c(11, 12))) %>% filter(!(Name == "Secondary Energy|Electricity|Demand"))
-      
-      plotstyle <- plotstyle %>%
-        rename(Name = X) %>%
-        select(Name, color)
-      
-      dataPlotPNGFilter <- dataPlotPNGFilter %>%
-        left_join(plotstyle, by = "Name")
-      
-      magpiePNG <- report[, , unique(dataPlotPNGFilter$Name)]
-      magpiePNG <- magpiePNG[regionsPNG, yearsPNG, ]
-      
-      qmagpiePNG <- as.quitte(magpiePNG)
-      # Budget <- as.quitte(report[,, c("Emissions|CO2|Budget1p5C.Gt CO2/yr",
-      #                                 "Emissions|CO2|Budget2C.Gt CO2/yr")])
-      # 
-      # # Keep only the two non-NA World budget values
-      # budget_lines <- Budget %>%
-      #   filter(
-      #     region == "World",
-      #     !is.na(value)
-      #   ) %>%
-      #   distinct(DSBSpEFS, value)
-      
-      # Original region codes and their displayed names
-      region_order <- c(
-        "World",
-        "EU",
-        "IND",
-        "CHA",
-        "USA"
-      )
-      
-      region_labels <- c(
-        "World" = "World",
-        "EU"    = "European Union",
-        "IND"   = "India",
-        "CHA"   = "China",
-        "USA"   = "USA"
-      )
-      
-      display_region_order <- unname(region_labels[region_order])
-      
-      # Join plotting information, including colors
-      plot_data <- qmagpiePNG %>%
-        filter(
-          DSBSpEFS %in% dataPlotPNGFilter$Name,
-          region %in% region_order,
-          !is.na(value)
-        ) %>%
-        left_join(
-          dataPlotPNGFilter %>%
-            distinct(Name, Variables, Group, color),
-          by = c("DSBSpEFS" = "Name")
-        ) %>%
-        mutate(
-          region = recode(
-            as.character(region),
-            !!!region_labels
-          ),
-          region = factor(
-            region,
-            levels = display_region_order
-          )
-        )
-      
-      # Titles for the PNGs
-      group_titles <- c(
-        "1"  = "CO2 Emissions",
-        "2"  = "Cumulated CO2 Emissions",
-        "3"  = "Final Energy by Sector",
-        "4"  = "Final Energy by Fuel",
-        "5"  = "Electricity Generation",
-        "6"  = "Electricity Generation by CCS",
-        "7"  = "Electricity Capacity",
-        "8"  = "Carbon Price",
-        "9"  = "Gross Inland Consumption",
-        "10" = "Gross CO2 Emissions"
-      )
-      
-      # Loop over groups
-      for (current_group in sort(unique(plot_data$Group))) {
-        
-        data_group <- plot_data %>%
-          filter(Group == current_group)
-        
-        if (nrow(data_group) == 0) next
-        
-        # Preserve legend and stacking order
-        variable_order <- dataPlotPNGFilter %>%
-          filter(Group == current_group) %>%
-          pull(Variables) %>%
-          unique()
-        
-        # Named color vector for the current group
-        group_colors <- dataPlotPNGFilter %>%
-          filter(Group == current_group) %>%
-          distinct(Variables, color) %>%
-          filter(!is.na(color), trimws(color) != "") %>%
-          {
-            setNames(.$color, .$Variables)
-          }
-        
-        data_group <- data_group %>%
-          mutate(
-            Variables = factor(
-              Variables,
-              levels = variable_order
+      purrr::imap(
+        plots_list,
+        function(plot_group, list_name) {
+          
+          # Each element contains one ggplot inside a list
+          p <- plot_group[[1]]
+          
+          # Use the plots_list element name as the title
+          p <- p +
+            ggplot2::labs(
+              title = list_name
             )
-          )
-        
-        unit_label <- data_group %>%
-          filter(!is.na(unit)) %>%
-          pull(unit) %>%
-          unique() %>%
-          first()
-        
-        p <- ggplot(
-          data_group,
-          aes(
-            x = period,
-            y = value,
-            fill = Variables
-          )
-        ) +
-          geom_area(
-            position = "stack",
-            alpha = 0.85,
-            colour = "white",
-            linewidth = 0.15
-          ) +
-          facet_wrap(
+          
+          # Remove the existing layers
+          p$layers <- list()
+          
+          # Change colour mapping to fill mapping
+          p$mapping$fill <- p$mapping$colour
+          p$mapping$colour <- NULL
+          
+          # Keep all region panels in one row with separate y-axes
+          p$facet <- ggplot2::facet_wrap(
             ~region,
             nrow = 1,
             scales = "free_y"
-          ) +
-          labs(
-            title = group_titles[as.character(current_group)],
-            x = "Year",
-            y = unit_label,
-            fill = NULL
-          ) +
-          scale_fill_manual(
-            values = group_colors,
-            breaks = variable_order,
-            drop = FALSE
-          ) +
-          scale_x_continuous(
-            breaks = scales::pretty_breaks(6)
-          ) +
-          scale_y_continuous(
-            labels = scales::label_number(big.mark = ","),
-            expand = expansion(mult = c(0, 0.05))
-          ) +
-          theme_bw(base_size = 15) +
-          theme(
-            plot.title = element_text(
-              face = "bold",
-              size = 18,
-              hjust = 0.5
-            ),
-            strip.text = element_text(
-              face = "bold",
-              size = 13
-            ),
-            strip.background = element_rect(fill = "grey90"),
-            axis.title = element_text(
-              face = "bold",
-              size = 13
-            ),
-            axis.text = element_text(size = 11),
-            panel.grid.minor = element_blank(),
-            legend.position = "bottom",
-            legend.title = element_blank(),
-            legend.text = element_text(size = 11),
-            legend.key.width = unit(1.2, "cm"),
-            plot.margin = ggplot2::margin(10, 10, 10, 10)
-          ) +
-          guides(
-            fill = guide_legend(
-              nrow = ceiling(length(variable_order) / 5),
-              byrow = TRUE
-            )
           )
-        # Add the two dashed budget lines only to
-        # the Emissions|CO2|Cumulated plot
-        # if (any(as.character(data_group$DSBSpEFS) == "Emissions|CO2|Cumulated")) {
-        #   
-        #   world_budget_lines <- budget_lines %>%
-        #     mutate(region = "World")
-        #   
-        #   p <- p +
-        #     geom_hline(
-        #       data = world_budget_lines,
-        #       aes(
-        #         yintercept = value,
-        #         linetype = DSBSpEFS
-        #       ),
-        #       colour = "black",
-        #       linewidth = 0.8,
-        #       inherit.aes = FALSE
-        #     ) +
-        #     scale_linetype_manual(
-        #       values = c(
-        #         "Emissions|CO2|Budget1p5C" = "dashed",
-        #         "Emissions|CO2|Budget2C"   = "dashed"
-        #       ),
-        #       name = NULL
-        #     )
-        # }
-        
-        file_name <- paste0(
-          sprintf("%02d", current_group),
-          "_",
-          str_replace_all(
-            group_titles[as.character(current_group)],
-            "[^A-Za-z0-9]+",
-            "_"
-          ),
-          ".png"
-        )
-        
-        ggsave(
-          filename = file.path(output_dir, file_name),
-          plot = p,
-          width = 18,
-          height = 6,
-          dpi = 300,
-          bg = "white"
-        )
-      }
+          
+          # Add stacked area geometry
+          p <- p +
+            ggplot2::geom_area(
+              position = "stack",
+              alpha = 0.85,
+              colour = "white",
+              linewidth = 0.15
+            ) +
+            ggplot2::labs(
+              colour = NULL,
+              fill = NULL
+            ) +
+            ggplot2::theme(
+              plot.title = ggplot2::element_text(
+                face = "bold",
+                size = 18,
+                hjust = 0.5
+              ),
+              legend.position = "bottom"
+            )
+          
+          file_name <- paste0(
+            stringr::str_replace_all(
+              list_name,
+              "[^A-Za-z0-9]+",
+              "_"
+            ),
+            ".png"
+          )
+          
+          ggplot2::ggsave(
+            filename = file.path(
+              output_dir,
+              file_name
+            ),
+            plot = p,
+            width = 24,
+            height = 6,
+            dpi = 300,
+            bg = "white"
+          )
+          
+          invisible(NULL)
+        }
+      )
     }
   }
-  invisible(NULL)
 }
-  
