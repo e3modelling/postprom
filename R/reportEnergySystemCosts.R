9ccc3078aef5c1afbbf3619e0f5cb066bd7de859
Fsioutas <fsioutas@hotmail.com>
2026-09-02 15:32:53 +0300

    Rename RenametoSTECH function to RenametoTECH and update its implementation for consistency in investment and capital cost calculations

--- R/reportEnergySystemCosts.R
+++ R/reportEnergySystemCosts.R
@@ -41,11 +41,11 @@ reportEnergySystemCosts <- function(path, regions, years) {
     investment <- investment * imCGI
   }
   investment[!is.finite(investment)] <- 0
-  investment <- RenametoSTECH(path, investment)
+  investment <- RenametoTECH(path, investment)
   getItems(investment, 3) <- paste0("Investment|Energy Supply|Electricity|", getItems(investment, 3))
   investment <- add_dimension(investment, dim = 3.2, add = "unit", nm = "billion US$2015/yr")
 
-  capitalCost <- RenametoSTECH(path, capitalCost)
+  capitalCost <- RenametoTECH(path, capitalCost)
   getItems(capitalCost, 3) <- paste0("Capital Cost|Electricity|", getItems(capitalCost, 3))
   capitalCost <- add_dimension(capitalCost, dim = 3.2, add = "unit", nm = "US$2015/kW")
 
@@ -60,11 +60,17 @@ reportEnergySystemCosts <- function(path, regions, years) {
 }
 
 # Helper ----------------------------------------------------------------------------------------
-RenametoSTECH <- function(path, prod) {
-  STECH <- rgdx.set(path, "STECH", te = TRUE) %>%
-    filter(i %in% getItems(prod,3))
+RenametoTECH <- function(path, prod) {
+  mapSEC <- data.frame(
+    i = c("ATHLGN", "ATHCOAL", "ATHGAS", "ATHBMSWAS", "ATHBMSCCS", "ATHOIL",
+          "PGLHYD", "PGSHYD", "PGAWND", "PGSOL", "PGCSP", "PGOTHREN",
+          "PGANUC", "ATHCOALCCS", "ATHLGNCCS", "ATHGASCCS", "PGAWNO", "PGH2F"),
+    
+    te = c("Lignite|w/o CCS", "Coal|w/o CCS","Gas|w/o CCS", "Biomass|w/o CCS","Biomass|w/ CCS", "Oil",
+           "Hydro|Large", "Hydro|Small", "Wind|Onshore", "Solar|PV","Solar|CSP", "Geothermal",
+           "Nuclear","Coal|w/ CCS", "Lignite|w/ CCS","Gas|w/ CCS", "Wind|Offshore","Hydrogen"))
   
-  prod <- toolAggregate(prod, dim = 3, rel = STECH, from = "i", to = ".te")
+  prod <- toolAggregate(prod, dim = 3, rel = mapSEC, from = "i", to = "te")
   return(prod)
 }
 
